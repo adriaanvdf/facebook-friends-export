4025a9ecdb213084436ca4f7a88ffc125535457c
adriaanvdf <github@vdfeltz.com>
2022-07-08 15:03:58 +0200

    Removed unneeded entries from db utils and clean up utils formatting

--- utils.py
+++ utils.py
@@ -5,8 +5,6 @@ import os
 
 from sqlite_utils import Database
 
-# driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-
 # --- Paths ---
 db_folder = 'db/'
 json_folder = db_folder + 'json/'
@@ -23,20 +21,18 @@ db_path = db_folder + db_file
 
 db = Database(db_path)
 
+
 def db_setup():
     db["friend_list"].create({
-        'id':int,
+        'id': int,
     }, pk="id")
 
     db["profiles"].create({
-        'id':int,
-        }, pk="id")
-
-    db["locations"].create({
-        "location":str
-        }, pk="location")
+        'id': int,
+    }, pk="id")
 
     print('>> Database initialized (%s)' % db_path)
+
 
 def db_read(table):
     data = []
@@ -44,12 +40,15 @@ def db_read(table):
         data.append(row)
     return data
 
-def db_write(table,data):
+
+def db_write(table, data):
     db_table = db[table]
     db_table.insert(data, alter=True)
 
-def db_update(table,id,data):
+
+def db_update(table, id, data):
     db[table].update(id, data, alter=True)
+
 
 def db_to_json():
     tables = db.table_names()
@@ -59,8 +58,9 @@ def db_to_json():
             data.append(row)
         json_path = json_folder + table + '.json'
         with open(json_path, 'w+', encoding="utf-8") as f:
-                json.dump(data, f, indent=2)
-        print('%s extracted to %s' % (table,json_path))
+            json.dump(data, f, indent=2)
+        print('%s extracted to %s' % (table, json_path))
+
 
 # Initialize database if not yet created
 if len(db.table_names()) == 0:
